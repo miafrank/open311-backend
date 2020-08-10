45b57fbbd6c30c7fd29c3a721a125c57f4997737
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-08-10 16:24:09 -0500

    added setup script to create tables if they do not exist.

---
+++ setup_env.py
@@ -0,0 +1,33 @@
+import logging
+from os import environ
+
+from db import db_client
+import db_utils
+from config import *
+
+
+def create_tables():
+    existing_tables = db_client().list_tables()['TableNames']
+
+    if REQUESTS_TABLE_NAME not in existing_tables:
+        db_utils.create_table(REQUESTS_TABLE_NAME, REQUESTS_KEY_SCHEMA, REQUESTS_ATTR_DEF)
+    if SERVICES_TABLE_NAME not in existing_tables:
+        db_utils.create_table(SERVICES_KEY_SCHEMA, SERVICES_KEY_SCHEMA, SERVICES_ATTR_DEF)
+
+
+def setup():
+    aws_access_key_id = environ.get('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = environ.get('AWS_SECRET_ACCESS_KEY')
+    stl_open_311_api_key = environ['STL_API_KEY']
+
+    if not aws_access_key_id or not aws_secret_access_key:
+        raise EnvironmentError(logging.error("Missing access key id or secret access key AWS credential"))
+    if not stl_open_311_api_key:
+        raise EnvironmentError(logging.error("API key for STL Open 311 API missing."))
+
+    # If tables do not exist, create tables
+    create_tables()
+
+
+if __name__ == "__main__":
+    setup()

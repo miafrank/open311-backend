f2cb877cc8e1f960f868fa34d545a41d716df7b8
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-08-11 10:26:53 -0500

    added service definition method

--- db_utils.py
+++ db_utils.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 from os import environ
 
-from api_utils import get_requests_from_stl
+from api_utils import get_requests_from_stl, get_resource_response_with_id
 from db import db_client, db_resource
 from config import *
 
@@ -19,17 +19,34 @@ def create_table(table_name, key_schema, attr_def):
 def insert_item(table_name):
     stl_open_311_api_key = environ['STL_API_KEY']
 
-    # table name corresponds directly to resource (endpoint) name
+    # table name corresponds directly to resources "requests" and "services"
     response = get_requests_from_stl(STL_API_URL, table_name, stl_open_311_api_key)
     requests_table = db_resource().Table(table_name)
 
     for item in response:
         if table_name == 'requests':
-            # Store LAT, LONG as strings - DynamoDB cannot store numbers with precision points
+            # store LAT, LONG as strings - DynamoDB cannot store numbers with precision points
             item["LAT"] = str(item["LAT"])
             item["LONG"] = str(item["LONG"])
 
         requests_table.put_item(Item=item)
+
+
+def insert_item_with_id(table_name):
+    stl_open_311_api_key = environ['STL_API_KEY']
+
+    response = get_requests_from_stl(STL_API_URL, table_name, stl_open_311_api_key)
+    # filter out top level parent services with no service definition
+    service_definitions = [service for service in response if service["PARENT_SERVICE_CODE"] != 0]
+
+    # get a list of service codes
+    service_codes = list(map(lambda x: x["SERVICE_CODE"], service_definitions))
+    service_definition_table = db_resource().Table(table_name)
+
+    for service_code in service_codes:
+        # get service definition by id
+        item = get_resource_response_with_id(STL_API_URL, table_name, service_code)
+        service_definition_table.put_item(Item=item)
 
 
 def scan_table(table_name):

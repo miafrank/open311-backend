681994880699217e174bd713314df593c1b515ac
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-08-16 16:52:42 -0500

    updated set up script

--- setup_env.py
+++ setup_env.py
@@ -1,9 +1,9 @@
 import logging
 from os import environ
 
-from db import db_client
 from db_utils import *
 from config import *
+from api_utils import *
 
 
 def init_tables(stl_api_key):
@@ -14,20 +14,29 @@ def init_tables(stl_api_key):
 
         create_table(REQUESTS_TABLE_NAME, REQUESTS_KEY_SCHEMA, REQUESTS_ATTR_DEF)
 
-        insert_item(db_resource_table(REQUESTS_TABLE_NAME),
-                    get_requests_from_stl(STL_API_URL, REQUESTS_TABLE_NAME, stl_api_key))
+        insert_resource(db_resource_table(REQUESTS_TABLE_NAME),
+                        get_requests_from_stl(STL_API_URL, REQUESTS_TABLE_NAME, stl_api_key))
 
     if SERVICES_TABLE_NAME not in existing_tables:
         logging.info(f'Creating {SERVICES_TABLE_NAME} table and inserting data from STL API')
 
         create_table(SERVICES_TABLE_NAME, SERVICES_KEY_SCHEMA, SERVICES_ATTR_DEF)
-        insert_item(get_requests_from_stl(STL_API_URL, SERVICES_TABLE_NAME, stl_api_key))
+
+        insert_resource(db_resource_table(SERVICES_TABLE_NAME),
+                        get_requests_from_stl(STL_API_URL, SERVICES_TABLE_NAME, stl_api_key))
 
     if SERVICE_DEFINITIONS_TABLE_NAME not in existing_tables:
         logging.info(f'Creating {SERVICE_DEFINITIONS_TABLE_NAME} table and inserting data from STL API')
 
         create_table(SERVICE_DEFINITIONS_TABLE_NAME, SERVICE_DEFINITIONS_KEY_SCHEMA, SERVICE_DEFINITIONS_ATTR_DEF)
-        insert_item(get_requests_from_stl(STL_API_URL, SERVICE_DEFINITIONS_TABLE_NAME, stl_api_key))
+        service_codes = get_child_service_codes(get_requests_from_stl(STL_API_URL, SERVICES_TABLE_NAME, stl_api_key))
+
+        for service_code in service_codes:
+            insert_resources_with_id(db_resource_table(SERVICE_DEFINITIONS_TABLE_NAME),
+                                     get_resource_response_with_id(STL_API_URL,
+                                                                   SERVICES_RESOURCE,
+                                                                   service_code,
+                                                                   stl_api_key))
 
 
 def setup():

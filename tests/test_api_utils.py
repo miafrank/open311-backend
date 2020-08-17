223a8d8e0f8971a6d9e71097395caa0e8f107fea
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-08-17 11:11:36 -0500

    updated fakeendpoint

--- tests/test_api_utils.py
+++ tests/test_api_utils.py
@@ -1,16 +1,16 @@
 from api_utils import *
 
 
-api_endpoint, resource, api_key = 'endpoint/', 'services', 'api_key'
+api_endpoint, resource, api_key = 'http://endpoint/', 'services', 'api_key'
 
 
 def test_build_open311_resources_url():
     url = build_open311_resources_url(api_endpoint, resource, api_key)
-    expected = 'endpoint/services.json?api_key=api_key'
+    expected = 'http://endpoint/services.json?api_key=api_key'
     assert url == expected
 
 
 def test_build_open311_resources_url_with_id():
     url = build_open311_resources_url_with_id(api_endpoint, resource, '1500', api_key)
-    expected = 'endpoint/services/1500.json?api_key=api_key'
+    expected = 'http://endpoint/services/1500.json?api_key=api_key'
     assert url == expected

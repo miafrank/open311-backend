1e36d98dab989f77142d4ea17d1b2f7d3ddcad77
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-08-17 10:22:59 -0500

    added tests for create table and child service codes

--- tests/test_db_utils.py
+++ tests/test_db_utils.py
@@ -1,10 +1,11 @@
-from moto import mock_dynamodb2
-import pytest
 import json
 from http import HTTPStatus
 
-from db_utils import *
+import pytest
+from moto import mock_dynamodb2
+
 from config import *
+from db_utils import *
 
 requests_table_name, services_table_name, service_definition_name = (REQUESTS_TABLE_NAME,
                                                                      SERVICES_TABLE_NAME,
@@ -13,8 +14,8 @@ requests_table_name, services_table_name, service_definition_name = (REQUESTS_TA
 
 @mock_dynamodb2
 def dynamodb_setup(table_name, key_schema, attr_def):
-    dynamodb = boto3.resource('dynamodb', 'us-east-1')
-    dynamodb.create_table(
+    db = boto3.resource('dynamodb', 'us-east-1')
+    db.create_table(
         TableName=table_name,
         KeySchema=key_schema,
         AttributeDefinitions=attr_def,
@@ -23,13 +24,26 @@ def dynamodb_setup(table_name, key_schema, attr_def):
             'WriteCapacityUnits': 1
         }
     )
-    return dynamodb.Table(table_name)
+    return db.Table(table_name)
 
 
 def load_resource(resource_path):
     with open(resource_path) as resource:
         resource = json.load(resource)
     return resource
+
+
+@mock_dynamodb2
+@pytest.mark.parametrize("table_name, key_schema, attr_def",
+                         [(requests_table_name,
+                           REQUESTS_KEY_SCHEMA,
+                           REQUESTS_ATTR_DEF),
+                          (services_table_name,
+                           SERVICES_KEY_SCHEMA,
+                           SERVICES_ATTR_DEF)])
+def test_create_table(table_name, key_schema, attr_def):
+    table = create_table(table_name, key_schema, attr_def)
+    assert table == boto3.resource('dynamodb').Table(name=table_name)
 
 
 @mock_dynamodb2
@@ -74,3 +88,11 @@ def test_insert_item_with_id(table_name, key_schema, attr_def, service_codes, se
                                     service_definition_response)
 
     assert item['ResponseMetadata']['HTTPStatusCode'] == expected
+
+
+@pytest.mark.parametrize("response, expected",
+                         [(load_resource('tests/services.json'), 2),
+                          ([], 0)])
+def test_child_service_codes(response, expected):
+    service_codes = get_child_service_codes(response)
+    assert len(service_codes) == expected

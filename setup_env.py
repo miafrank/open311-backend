c09a2222bfc34428ccafab8a4db3817d0f5b2d31
Mia Frank <mia.frank@madeintandem.com>
2021-08-05 10:30:03 -0500

    refactored setup script

--- setup_env.py
+++ setup_env.py
@@ -6,16 +6,16 @@ from db import *
 
 def init_tables():
     create_tables(REQUESTS_TABLE_NAME, REQUESTS_KEY_SCHEMA, REQUESTS_ATTR_DEF)
-    insert_resource(db_table(REQUESTS_TABLE_NAME), get_requests_from_stl(STL_API_URL, REQUESTS_TABLE_NAME))
+    insert_resource(db_table(REQUESTS_TABLE_NAME), fetch_all(REQUESTS_TABLE_NAME))
 
     create_tables(SERVICES_TABLE_NAME, SERVICES_KEY_SCHEMA, SERVICES_ATTR_DEF)
-    insert_resource(db_table(SERVICES_TABLE_NAME), get_requests_from_stl(STL_API_URL, SERVICES_TABLE_NAME))
+    insert_resource(db_table(SERVICES_TABLE_NAME), fetch_all(SERVICES_TABLE_NAME))
 
     create_tables(SERVICE_DEFINITIONS_TABLE_NAME, SERVICE_DEFINITIONS_KEY_SCHEMA, SERVICE_DEFINITIONS_ATTR_DEF)
-    service_codes = get_child_service_codes(get_requests_from_stl(STL_API_URL, SERVICES_TABLE_NAME))
+    service_codes = get_child_service_codes(fetch_all(SERVICES_TABLE_NAME))
 
     for service_code in service_codes:
-        service_def = get_resource_by_id(STL_API_URL, SERVICES_RESOURCE, service_code)
+        service_def = fetch_by_id(STL_API_URL, service_code)
         insert_resources_with_id(db_table(SERVICE_DEFINITIONS_TABLE_NAME), service_def)
 
 

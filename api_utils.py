e62080efb4744b6655e2b13a8f59d64e5d519483
Mia Frank <mia.frank@madeintandem.com>
2021-08-05 10:28:55 -0500

    refactored api utils

--- api_utils.py
+++ api_utils.py
@@ -1,29 +1,29 @@
 import requests
 from http import HTTPStatus
+from env import api_key, base_api_url, api_headers
 
 
-def build_open311_resources_url(api_endpoint, resource, api_key):
-    return f'{api_endpoint}{resource}.json?api_key={api_key}'
+def fetch_all(resource):
+    url = build_url(resource=resource)
+    return requests.get(url).json()
 
 
-def build_open311_resources_url_with_id(api_endpoint, resource, service_code, api_key):
-    return f'{api_endpoint}{resource}/' \
-           f'{service_code}.json' \
-           f'?api_key={api_key}'
-
-
-def get_requests_from_stl(api_endpoint, resource, api_key):
-    url = build_open311_resources_url(api_endpoint, resource, api_key)
-    resp = requests.get(url).json()
-    return resp
-
-
-def get_resource_by_id(api_endpoint, resource, service_code, api_key):
-    url = build_open311_resources_url_with_id(api_endpoint, resource, service_code, api_key)
+def fetch_by_id(resource, code):
+    url = build_url(resource=resource, code=code)
     response = requests.get(url).json()
+
     if 'CODE' in response and response['CODE'] == HTTPStatus.NOT_FOUND:
-        # TODO - confirm this behavior
+        # TODO - Save parent service code
         # STL API does not return HTTP.404 when service code not found or service code is parent service code
         # parent service codes do not have a service definition. Return response as service code with empty attr
-        return {"SERVICE_CODE": service_code, "ERROR": {HTTPStatus.NOT_FOUND}}
-    return response
+        return {"SERVICE_CODE": code, "ERROR": {HTTPStatus.NOT_FOUND}}
+    else:
+        # TODO - Save resource (service def, request, or service)
+        return response
+
+
+def build_url(**kwargs):
+    if kwargs['code']:
+        return f'{base_api_url}{kwargs["resource"]}/{kwargs["code"]}{api_headers}{api_key}'
+    else:
+        return f'{base_api_url}{kwargs["resource"]}{api_headers}{api_key}'

4c1f2bb05926a32fd53c1f65ada8517b22493192
Mia Frank <mia.frank@madeintandem.com>
2021-08-06 10:40:31 -0500

    added test env file. pytest dotenv. and updated env helper

--- env.py
+++ env.py
@@ -9,8 +9,6 @@ def setup_env():
     try:
         open('env', 'r')
         load_dotenv()
-        aws_access_key_id()
-        aws_secret_access_key()
     except(IOError, ValueError):
         logging.error('Cannot set application environment variables.')
 
@@ -18,26 +16,26 @@ def setup_env():
 def _get_env(env_name, msg):
     env = getenv(env_name)
     if env:
-        return str(env)
+        return env
     else:
         raise KeyError(logging.error(msg))
 
 
 def aws_access_key_id():
-    _get_env('AWS_ACCESS_KEY_ID', 'Missing access key id or secret access key AWS credential')
+    return _get_env('AWS_ACCESS_KEY_ID', 'Missing access key id or secret access key AWS credential')
 
 
 def aws_secret_access_key():
-    _get_env('AWS_SECRET_ACCESS_KEY', 'Missing access key id or secret access key AWS credential')
+    return _get_env('AWS_SECRET_ACCESS_KEY', 'Missing access key id or secret access key AWS credential')
 
 
 def api_key():
-    _get_env('STL_API_KEY', 'Missing API key')
+    return _get_env('STL_API_KEY', 'Missing API key')
 
 
 def base_api_url():
-    _get_env('BASE_API_URL', 'Missing base url')
+    return _get_env('BASE_API_URL', 'Missing base url')
 
 
 def api_headers():
-    _get_env('API_HEADERS', 'Missing API Headers')
+    return _get_env('API_HEADERS', 'Missing API Headers')
--- tests/test_api_utils.py
+++ tests/test_api_utils.py
@@ -1,16 +1,15 @@
 from api_utils import build_url
 
-
 resource, code = 'services', '15'
 
 
 def test_build_open311_resources_url():
     url = build_url(resource=resource)
-    expected = 'https://www.stlouis-mo.gov/powernap/stlouis/api.cfm/services.json?api_key=api_key'
+    expected = 'https://test_api.com/services.json?api_key=123'
     assert url == expected
 
 
 def test_build_open311_resources_url_with_id():
     url = build_url(resource=resource, code=code)
-    expected = 'https://www.stlouis-mo.gov/powernap/stlouis/api.cfm/services/1500.json?api_key=api_key'
+    expected = 'https://test_api.com/services/15.json?api_key=123'
     assert url == expected

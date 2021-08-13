81d2984f5bbd66435f3685a08173f96a8f345c6d
Mia Frank <mia.frank@madeintandem.com>
2021-08-13 11:05:57 -0500

    added sqlalchemy and added data model for service and service def

---
+++ models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, Integer, String, Date, Boolean, Enum, ARRAY
+from sqlalchemy.orm import as_declarative, declared_attr
+
+
+@as_declarative()
+class Base(object):
+    @declared_attr
+    def __tablename__(cls):
+        # TODO - can cls be changed to self? Why did the docs say this lol
+        return cls.__name__.lower()
+
+    id = Column(Integer)
+
+
+# Service - A Single Service
+class Service(Base):
+    # TODO - associate with Service Definition if meta true
+    service_code = Column(String, nullable=False, primary_key=True)
+    service_name = Column(String, nullable=False)
+    description = Column(String)
+    # TODO - if true this indicates a parent service with a definition
+    metadata = Column(Boolean)
+    # This is a String in original spec but it's a set of Enums
+    type = Column(Enum, nullable=False)
+    keywords = Column(String)
+    group = Column(String)
+
+
+# Service Definition - Attributes associated with a service code
+class ServiceDefinition(Base):
+    service_code = Column(String, nullable=False, primary_key=True)
+    attributes = Column(ARRAY, nullable=False)
+
+# Service Attribute - A single attribute extension for a service
+# New Service Request
+# Request Response
